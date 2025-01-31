0a98e7d5a69c7964913a3631880c13f0c84bd156
HarrySeop <141125424+HarrySeop@users.noreply.github.com>
2025-01-31 14:43:14 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,15 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+const a = Number(input[0]);
+const b = Number(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
