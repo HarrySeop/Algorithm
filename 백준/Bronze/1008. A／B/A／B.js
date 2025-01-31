8c54046609c4e87e2899d2f92074bc4cbc44a9cd
HarrySeop <141125424+HarrySeop@users.noreply.github.com>
2025-01-31 14:39:40 +0900

    [Bronze V] Title: A/B, Time: 96 ms, Memory: 9612 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,8 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+console.log(Number(input[0]) / Number(input[1]));
