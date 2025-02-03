7d4d393092475d1035cded1e2f86194e6f0a917e
HarrySeop <141125424+HarrySeop@users.noreply.github.com>
2025-02-03 23:54:43 +0900

    [Bronze V] Title: A+B - 8, Time: 100 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,16 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(el => el.split(' ').map(Number));
+
+const T = input[0][0];
+
+for (let i = 1; i <= T; i++) {
+  console.log(
+    `Case #${i}: ${input[i][0]} + ${input[i][1]} = ${
+      input[i][0] + input[i][1]
+    }`,
+  );
+}
