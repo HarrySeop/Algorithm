a5c48052acccaf035f8fc649149572159c84284f
HarrySeop <141125424+HarrySeop@users.noreply.github.com>
2025-02-09 23:44:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,7 @@
+const [word, i] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+console.log(word.charAt(i - 1));
