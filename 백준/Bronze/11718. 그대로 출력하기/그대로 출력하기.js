61bed5f3fd278226dabc507fa80fd131c37b0e55
HarrySeop <141125424+HarrySeop@users.noreply.github.com>
2025-02-10 17:54:02 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 9500 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,7 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+input.forEach(v => console.log(v));
