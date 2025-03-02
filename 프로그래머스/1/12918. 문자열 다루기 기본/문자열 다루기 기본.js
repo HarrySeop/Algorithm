a942de63cdd43b0d4127590834fb5d5ab5b3a7c1
HarrySeop <141125424+HarrySeop@users.noreply.github.com>
2025-03-03 01:41:55 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    if(s.length !== 4 && s.length !== 6) {
+        return false;
+    }
+    if(isNaN(s)) {
+        return false;
+    }
+    if(s.includes('e') || s.includes('E')) {
+        return false;
+    }
+    
+    return true;
+}
